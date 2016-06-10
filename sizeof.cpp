672f601bbfd54c8c6c0b3202d69e29d882d15520
Chi Yung Fung (Jimmy) <jimmythefung@users.noreply.github.com>
2016-06-10 14:23:31 -0500

    Sample output in comment

--- sizeof.cpp
+++ sizeof.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main(){
-
-    void *p;
-
-    cout << "size of bool: "        << sizeof(bool)      << " bytes." << endl;
-    cout << "size of char: "        << sizeof(char)      << " bytes." << endl;
-    cout << "size of short: "       << sizeof(short)     << " bytes." << endl;
-    cout << "size of int: "         << sizeof(int)       << " bytes." << endl;
-    cout << "size of long: "        << sizeof(long)      << " bytes." << endl;
-    cout << "size of long long: "   << sizeof(long long) << " bytes." << endl;
-    cout << "size of float: "       << sizeof(float)     << " bytes." << endl;
-    cout << "size of double: "      << sizeof(double)    << " bytes." << endl;
-    cout << "size of long double: " << sizeof(long double) << " bytes." << endl;
+                                                                                  // Ubuntu 16.04 LTS, Core i5-3550 16GB
+    cout << "size of bool: "        << sizeof(bool)        << " bytes." << endl;  // 1 bytes
+    cout << "size of char: "        << sizeof(char)        << " bytes." << endl;  // 1 bytes
+    cout << "size of short: "       << sizeof(short)       << " bytes." << endl;  // 2 bytes
+    cout << "size of int: "         << sizeof(int)         << " bytes." << endl;  // 4 bytes
+    cout << "size of long: "        << sizeof(long)        << " bytes." << endl;  // 8 bytes
+    cout << "size of long long: "   << sizeof(long long)   << " bytes." << endl;  // 8 bytes
+    cout << "size of float: "       << sizeof(float)       << " bytes." << endl;  // 4 bytes
+    cout << "size of double: "      << sizeof(double)      << " bytes." << endl;  // 8 bytes
+    cout << "size of long double: " << sizeof(long double) << " bytes." << endl;  // 16 bytes
     
-    cout << "size of void pointer: " << sizeof(p) << " bytes." << endl;
+    void *p;
+    cout << "size of void pointer: " << sizeof(p)          << " bytes." << endl;  // 8 bytes
     return 0;
 }
