50570b7f7ecb3618d3ad0a5d3c746f50c03ed68c
Jimmy <jimmythefung@yahoo.com>
2016-06-13 02:07:14 -0500

    implemented swap

--- EPI_Problems/6.1/main.cpp
+++ EPI_Problems/6.1/main.cpp
@@ -3,9 +3,23 @@
 #include <vector>
 using namespace std;
 
-
+void swap(int *left, int *right);
 
 int main(){
-    vector<int> A{0,1,2,3,4,5};
+    vector<int> A{0,1,2,3};
+    
+    // print original list
+    cout << A[0] << A[1] << A[2] << A[3] << endl;
+    
+    // swap pointer
+    swap(&A[0], &A[3]);
+    cout << A[0] << A[1] << A[2] << A[3] << endl;
     return 0;
+}
+
+void swap(int *left, int *right){
+    int temp;
+    temp = *left;
+    *left = *right;
+    *right = temp;
 }
