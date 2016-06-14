6fe9f5eb73770ed42e393d43abe7a77a5fb11eb6
jimmy <jimmythefung@yahoo.com>
2016-06-14 08:42:08 -0700

    p6.6 buy and sell stock once

---
+++ EPI_Problems/6.6/main.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h> /* srand, rand */
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/* Function Prototyes */
+
+
+int main(){
+
+    return 0;
+}
