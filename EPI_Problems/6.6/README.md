defa8dde0c42ca26935c4cfccc6214c441a949e4
jimmy <jimmythefung@yahoo.com>
2016-06-14 11:41:10 -0700

    O(n) solution implemented

--- EPI_Problems/6.6/README.md
+++ EPI_Problems/6.6/README.md
@@ -1,1 +1,9 @@
-# Buy and Sell a Stock Once  
+## Buy and Sell a Stock Once  
+Straght forward O(n) complexity, O(1) space implementation exists. Usually arrived at after gaining the insight from trying out divide and conqure.  
+The insight is that keeping track of the minimum seen so far and the best profit obtained so far will yield answer in 1 pass of input.  
+
+## Reference note: this relates to the CLRS's section 4.1, p.68, "The maximum subarray problem".  
+Although not the simpler solution, this is a good exercise to review divide and conqure technique, and solving continous valued liked problems.
+The Maximum subarray problem is about finding the greatest sum in a continous interval of a subarray.  
+**The solution of greatest sum is only relavent** when the prices of stocks each day are converted to delta of prices, and so the input become the latter.  
+
--- EPI_Problems/6.6/main.cpp
+++ EPI_Problems/6.6/main.cpp
@@ -1,13 +1,51 @@
 #include <stdlib.h> /* srand, rand */
+#include <cmath>    /* abs()       */
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-/* Function Prototyes */
+vector<int> prices_CLRS = {100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
+vector<int> prices_EPI = {310, 315, 275, 295, 260, 270, 290, 230, 255, 250};
 
+
+/* Function Prototyes */
+int getProfit(vector<int>& A);
+void printv(vector<int>& A);
 
 int main(){
 
+    printv(prices_CLRS);
+    cout << "Profit:  " << getProfit(prices_CLRS) << endl;
+    printv(prices_EPI);
+    cout << "Profit:  " << getProfit(prices_EPI) << endl;
+
     return 0;
+}
+
+int getProfit(vector<int>& A){
+    int currentProfit;
+    int bestProfit=0;      // not buying and selling beats buying then sold for a loss, for starter.
+    int minPriceSeen=A[0]; // because the loop below starts at i=1, the 2nd element.
+    
+    for(int i=1; i < A.size(); i++){
+
+        currentProfit = A[i] - minPriceSeen;
+        if (currentProfit > bestProfit){
+            bestProfit = currentProfit;
+        }
+
+        if (A[i] < minPriceSeen){
+            minPriceSeen = A[i];   
+        }
+    }
+    return bestProfit;
+}
+
+void printv(vector<int>& A){
+    vector<int>::iterator it;
+    for(it=A.begin(); it<A.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
 }
