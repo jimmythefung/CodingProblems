f1f4a4274961916d85850213b09d1188fc6a23e0
jimmy <jimmythefung@yahoo.com>
2016-06-15 07:48:40 -0700

    support sign

--- EPI_Problems/7.1/main.cpp
+++ EPI_Problems/7.1/main.cpp
@@ -9,14 +9,19 @@ using namespace std;
 
 /* Function Prototyes */
 void printv(vector<int>& A);
+string myitos(int x);
 int mystoi(string s);
 int getint(char c);
 
 int main(){
-    string s = "65550";
+    string s = "-65550";
     cout << "Input string: " << s << " is type: " << typeid(s).name() << endl;
+    // string to integer
     cout << "mystoi output: "  << mystoi(s) << " is type: " << typeid(mystoi(s)).name() << endl;
 
+    // integer to string
+
+    
     /*
     int c;
     c = s[0]-'0';
@@ -37,17 +42,32 @@ void printv(vector<int>& A){
 }
 
 
+
+
 int mystoi(string s){
     int exp;
     unsigned place;  // s.size() is unsigned. So use unsigned for place
     int result=0;
+    string significant="";
+    int signMultiplier=1;
 
-    exp = s.size()-1;
-    for(place=0; place < s.size(); place++){
-        result = result + getint(s[place])*pow(10, exp);
+    // evaluate the sign and the significant part of the string
+    if (s[0] == '-'){
+        signMultiplier = -1;
+        significant    = s.substr(1, string::npos);
+    }else{
+        signMultiplier = 1;
+        significant    = s;
+    }
+    
+    // start conversion
+    exp = significant.size()-1;
+    for(place=0; place < significant.size(); place++){
+        result = result + getint(significant[place])*pow(10, exp);
         exp--;
     }
-    return result;
+
+    return signMultiplier*result;
 }
 
 int getint(char c){
