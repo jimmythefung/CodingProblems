b9458b24c3e28a5495b489b98a2f3faaaf7a415d
jimmy <jimmythefung@yahoo.com>
2016-06-17 14:17:25 -0700

    p8.1 added

---
+++ EPI_Problems/8.1/main.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h> /* srand, rand */
+#include <cmath>    /* abs()       */
+#include <typeinfo>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+/* Function Prototyes */
+void printv(vector<int>& A);
+
+int main(){
+    return 0;
+}
+
+void printv(vector<int>& A){
+    vector<int>::iterator it;
+    for(it=A.begin(); it<A.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
---
+++ EPI_Problems/8.1/mylist.h
@@ -0,0 +1,28 @@
+/*
+ * mylist.h
+ */
+
+#ifndef __MYLIST_H__
+#define __MYLIST_H__
+
+using namespace std;
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+
+class MyList {
+    public:
+
+    // default constructor
+
+    // Constructor
+    
+    // Copy Constructor
+
+    // Destructor
+};
+
+#endif
