8e150c29ec385aa9535712400663baa232039d21
jimmy <jimmythefung@yahoo.com>
2016-06-22 14:26:46 -0700

    BST implemented, need delete

--- EPI_Problems/10.1/main.cpp
+++ EPI_Problems/10.1/main.cpp
@@ -14,20 +14,61 @@ using namespace std;
 struct tNode{
     int data;
     tNode *L, *R;
+
+    tNode(int x){
+        data = x;
+        L = NULL;
+        R = NULL;
+    }
+
 };
 
 struct BST{
-    tNode* root;
+    tNode* root=NULL;
+
+    // default constructor
+    BST(){}
 
     // constructor
-    BST(){
-        root = NULL;
+    BST(vector<int> v){
+        for (auto it=v.begin(); it!=v.end(); it++){
+            insert(*it);
+        }
     }
 
     // destructor
+    ~BST(){
+        removeTree();
+    }
 
-    /* Method */
+    // Insert
+    void insert(int x){
+        RInsert(root, x);
+    }
+    void RInsert(tNode* &n, int x){
+        // empty tree
+        if (n==NULL){
+            n = new tNode(x); // requires (tNode* &n)
+        }
+        // x is smaller
+        else if (x < n->data){
+            if (n->L!=NULL){
+                RInsert(n->L, x);
+            }else{
+                n->L = new tNode(x);
+            }
+        }
+        // x is equal or bigger
+        else{
+            if (n->R!=NULL){
+                RInsert(n->R, x);
+            }else{
+                n->R = new tNode(x);
+            }
+        }
+    }
 
+    // traverse
     void traverse( void (*f)(tNode*) ){
         queue<tNode*> q;
         tNode *n;
@@ -37,46 +78,46 @@ struct BST{
 
         while (!q.empty()){
             n = q.front();
-            f(n);
-            q.pop();
             if (n->L!=NULL){
                 q.push(n->L);
             }
             if (n->R!=NULL){
                 q.push(n->R);
             }
+            q.pop();
+            f(n);
         }
     }
 
-    static void printNode(tNode* n){
-        cout << n->data << endl;
+    static void printNode(tNode* n){ // static because traverse takes function from global namespace
+        cout <<  n->data << " ";
     }
 
     void printTree(){
+        cout << "print Tree: ";
         traverse(printNode);
+        cout << endl;
+    }
+
+    static void removeNode(tNode* n){
+        delete n;
+    }
+
+    void removeTree(){
+        traverse(removeNode);
+        root = NULL;
     }
 };
 
 
-
-
 int main(){
-    vector<int> v = {4,6,1,2,3,9,7,5,3,4,0};
-    
-    queue<int> q;
-    q.push(1);
-    q.push(2);
 
-    cout << q.front() << endl;
-    q.pop();
-
-    cout << q.front() << endl;
-    q.pop();
-    
-    cout <<"empty? " << q.empty() << endl;
-    
     // BST
-    BST* t = new BST();
+    vector<int> v = {4,6,1,2,3,9,7,5,3,4,0};
+    BST* t = new BST(v);
+    t->printTree();
     
+    // clean up
+    delete t;
     return 0;
 }
