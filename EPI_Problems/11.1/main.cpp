fec76180a63de933d013f77ccdc0d295a933952b
Jimmy <jimmythefung@yahoo.com>
2016-06-24 21:39:23 -0500

    built matrix

--- EPI_Problems/11.1/main.cpp
+++ EPI_Problems/11.1/main.cpp
@@ -6,9 +6,22 @@
 #include <queue>
 using namespace std;
 
+void printMatrix(vector<vector<int>> &m){
+    for(vector<int> v : m){
+        for(int ele : v){
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 int main(){
+    vector<int> v1 = {0, 5, 8};
+    vector<int> v2 = {1, 9, 13, 19};
+    vector<int> v3 = {4, 2, 8, 6};
 
+    vector<vector<int>> m = {v1, v2, v3};
+    printMatrix(m);
     return 0;
 }
