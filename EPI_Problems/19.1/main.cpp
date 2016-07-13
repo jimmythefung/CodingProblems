c69d2ad5ee85c684db06a7180189650377b763dc
jimmy <jimmythefung@yahoo.com>
2016-07-13 14:35:39 -0700

    problem 19.1 template

---
+++ EPI_Problems/19.1/main.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h> /* srand, rand */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <list>
+using namespace std;
+
+template <typename T> 
+void printv2(vector<T> v, const string &s=string("Printing vector: ")){
+    // s is a const& - constant reference;
+    //   -content of s is constant; content can not be changed through s 
+    //   -s is a reference, so no copying
+    //   -In effect: Use s directly with no danger of mutation
+
+    cout << s;
+    for(auto it=v.begin(); it!=v.end(); it++){
+        cout << *it << ", ";
+    }
+    cout << endl;
+}
+
+int main(){
+    return 0;
+}
+
