38eec8f74b8427a72102952ffbb47dbca9a4de32
Jimmy Fung <jimmyfung@us.ibm.com>
2016-07-16 18:54:57 -0500

    formatted codes and added readme

--- EPI_Problems/stringPermutation/README.md
+++ EPI_Problems/stringPermutation/README.md
@@ -1,1 +1,13 @@
-# 
+# String Permutation  
+Given a string, s. Return all permutations.  
+i.e. s="abc"  
+     return {abc, acb, bac, bca, cab, cba}  
+
+Idea:  
+-Recursion.  
+-Let's say I have s="abc", and a permutation function P(s) that gives permutations of s. Then P(s) = "a"+{P("bc")}  
+-Note P("bc") = {"bc", "cb"}
+-Then for each element in {P("bc")}, insert "a" at all and incremental positions to form a new set {P("abc")}.  
+-We form, for "bc" -> (a)bc, b(a)c, bc(a)  
+-and for      "cb" -> (a)cb, c(a)b, cb(a)  
+-Which results in P(s) = {abc, bac, bca, acb, cab, cba}
--- EPI_Problems/stringPermutation/main.cpp
+++ EPI_Problems/stringPermutation/main.cpp
@@ -9,29 +9,29 @@ using namespace std;
 
 template <typename T> 
 void printv2(vector<T> v, const string &s=string("Printing vector: ")){
-	// s is a const& - constant reference;
-	//   -content of s is constant; content can not be changed through s 
-	//   -s is a reference, so no copying
-	//   -In effect: Use s directly with no danger of mutation
+    // s is a const& - constant reference;
+    //   -content of s is constant; content can not be changed through s 
+    //   -s is a reference, so no copying
+    //   -In effect: Use s directly with no danger of mutation
 
-	cout << s;
-	for(auto it=v.begin(); it!=v.end(); it++){
-		cout << *it << ", ";
-	}
-	cout << endl;
+    cout << s;
+    for(auto it=v.begin(); it!=v.end(); it++){
+        cout << *it << ", ";
+    }
+    cout << endl;
 }
 vector<string> perm(string s){
-	vector<string> result;
+    vector<string> result;
 
-	if(s.size()==1){
-		result.push_back(s);
+    if(s.size()==1){
+        result.push_back(s);
         return result;
-	}
+    }
 
-	for(string sMinus1: perm(s.substr(1))){
-		for (int i=0; i <= sMinus1.size(); i++){
-			result.push_back(sMinus1.substr(0,i) + s[0] + sMinus1.substr(i, sMinus1.size()-i));
-		}
+    for(string sMinus1: perm(s.substr(1))){
+        for (int i=0; i <= sMinus1.size(); i++){
+            result.push_back(sMinus1.substr(0,i) + s[0] + sMinus1.substr(i, sMinus1.size()-i));
+        }
 
         /* 
          * if sMinus1 = {bc, cb}
@@ -39,13 +39,13 @@ vector<string> perm(string s){
          * (a)bc, b(a)c, bc(a)
          * (a)cb, c(a)b, cb(a)
          */
-	}
+    }
 
     return result;
 }
 int main(){
     string s = "abc";
     printv2(perm(s));
-	return 0;
+    return 0;
 }
 
