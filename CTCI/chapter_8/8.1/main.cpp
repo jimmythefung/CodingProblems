1db03a79ab8d4a05a446264aa23e8aa26a405090
Jimmy <jimmythefung@yahoo.com>
2016-10-23 22:00:06 -0500

    updated content

--- CTCI/chapter_8/8.1/main.cpp
+++ CTCI/chapter_8/8.1/main.cpp
@@ -24,8 +24,45 @@ void printv2(vector<T> v, const string &s=string("Printing vector: ")){
     cout << endl;
 }
 
+int nSteps(int n, unordered_map<int, int> &cache){
+
+    if (cache.count(n)!=0){
+        return cache[n];
+    }
+
+    int sum = 0;
+
+    if (n-1 >= 0){
+        sum = sum + nSteps(n-1, cache);
+    }
+
+    if (n-2 >= 0){
+        sum = sum + nSteps(n-2, cache);
+    }
+
+    if (n-3 >= 0){
+        sum = sum + nSteps(n-3, cache);
+    }
+
+    cache[n] = sum;
+    
+    return sum;
+}
+
+
+int nStepsHelper(int n){
+    unordered_map<int, int> cache;
+    cache[0] = 1;
+    return nSteps(n, cache);
+}
+
 int main(){
-    //
+
+    cout << "n=1: " << nStepsHelper(1) << endl;
+    cout << "n=2: " << nStepsHelper(2) << endl;
+    cout << "n=3: " << nStepsHelper(3) << endl;
+    cout << "n=4: " << nStepsHelper(4) << endl;
+
     return 0;
 }
 
