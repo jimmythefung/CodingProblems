869b6b9755bb4dd0d02a5f5bcb47d4999c1dccd8
Jimmy <jimmythefung@yahoo.com>
2016-10-25 01:03:01 -0500

    updated content

--- CTCI/chapter_8/8.2/main.cpp
+++ CTCI/chapter_8/8.2/main.cpp
@@ -6,7 +6,11 @@
 #include <queue>
 #include <list>
 #include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
+#include <utility>
+#include <deque>
+#include <set>
 
 using namespace std;
 
@@ -31,6 +35,91 @@ void printM(vector<vector<T>> M){
         printv2(*it, "");
     }
 }
+
+
+void pushNextMoves(pair<int,int> &p, deque<pair<int,int>> &q, set<pair<int,int>> &visited, vector<vector<int>> &M){
+    int w=M[0].size()-1;
+    int h=M.size()-1;
+
+    int r = p.first;
+    int c = p.second;
+
+    pair<int, int> nextCell;
+
+    // right
+    nextCell = make_pair(r, c+1);
+    if (visited.count(nextCell)==0 && r>=0 && r<=h && c+1>=0 && c+1<=w){
+        q.push_back(nextCell);
+    }
+
+    // left
+    nextCell = make_pair(r, c-1);
+    if (visited.count(nextCell)==0 && r>=0 && r<=h && c-1>=0 && c-1<=w){
+        q.push_back(nextCell);
+    }
+
+    // up
+    nextCell = make_pair(r-1, c);
+    if (visited.count(nextCell)==0 && r-1>=0 && r-1<=h && c>=0 && c<=w){
+        q.push_back(nextCell);
+    }
+    
+    // down
+    nextCell = make_pair(r+1, c);
+    if (visited.count(nextCell)==0 && r+1>=0 && r+1<=h && c>=0 && c<=w){
+        q.push_back(nextCell);
+    }
+
+}
+
+bool robot(vector<vector<int>> M){
+    pair<int, int> cell;
+    deque< pair<int, int> > qA;
+    deque< pair<int, int> > qB;
+    set< pair<int, int> > visitedA;
+    set< pair<int, int> > visitedB;
+    cell = make_pair(0,0);
+    qA.push_back(cell);
+    cell = make_pair(M.size()-1, M[0].size()-1);
+    qB.push_back(cell);
+
+    
+    int turn=0;
+    int qA_size = 0;
+    int qB_size = 0;
+    while(qA.size()!=0 || qB.size()!=0){
+
+        // A's turn
+        if(turn%2==0){
+            qA_size = qA.size();
+            
+            // get all next BFS node into qA
+            for(int i=0; i < qA_size; i++){
+
+                // reached solution!
+                if (visitedB.count(qA.front())!=0){
+                    // what return type do we want??
+                    return true;
+                }
+                else{
+                    pushNextMoves(qA.front(), qA, visitedA, M);
+                    visitedA.insert(qA.front());
+                    qA.pop_front();
+                }
+            }
+        }
+        else{
+        }
+
+        turn++;
+    }
+    
+
+    return 0;
+}
+
+
+
 
 int main(){
     vector<vector<int>> M;
