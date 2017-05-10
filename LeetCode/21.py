b5441ae9b62d8ced404981f086f1b9978363b4d3
Chi Yung Fung (Jimmy) <jimmythefung@users.noreply.github.com>
2017-05-10 08:05:59 -0500

    Update 21.py

--- LeetCode/21.py
+++ LeetCode/21.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
@@ -13,26 +13,39 @@ class Solution(object):
         """
         return merge(l1, l2)
 
-        
 def merge(l1, l2):
     if l1==None:
         return l2
     if l2==None:
         return l1
-    head = l1
-    while l1 != None:
-        x = l1.next
-        y = l2.next
-        l1.next = l2
-        l2.next = x
-        l1 = x
-        l2 = y
-    return head
+    sentinel = ListNode('sentinel')
+    pre = sentinel
+    # loop invariant precoditions
+    p1 = l1
+    p2 = l2
+    while p1!=None and p2!=None:
+        if p1.val < p2.val:
+            pre.next = p1
+            pre = pre.next
+            p1 = p1.next
+        else:
+            pre.next = p2
+            pre = pre.next
+            p2 = p2.next
+
+        if p1==None:
+            pre.next = p2
+        if p2==None:
+            pre.next = p1
+    return sentinel.next
+
 
 l1 = ListNode(1)
 a = ListNode(3)
 b = ListNode(5)
 a.next=b
-l1.next=a
 
+# test
+l1.next=a
 l2 = ListNode(0)
+l3 = merge(l1, l2)
